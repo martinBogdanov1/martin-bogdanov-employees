c939a4aa6ce7ffaceaac258e9bae44f451fcb9f2
martinBogdanov1 <martin.petrov1994@yahoo.com>
2022-05-21 23:10:29 +0300

    implement input file reading logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import './App.css';
+import { useState } from "react";
+import { usePapaParse } from 'react-papaparse';
 
 function App() {
+  const { readString } = usePapaParse();
+  const [error, setError] = useState("");
+  const [data, setData] = useState([])
+
+  const onChangeFileHandler = (e) => {
+    setError("");
+    const inputFile = e.target.files[0];
+    const fileType = inputFile?.type;
+
+    if (fileType != "text/csv") {
+      setError("Please enter CSV file.");
+      e.target.value = null;
+      return;
+    }
+
+    parseData(inputFile);
+
+    function parseData(data) {
+      readString(data, {
+        worker: true,
+        complete: (results) => {
+          setData(results.data); 
+        }
+      });
+    }
+  }
+
+
   return (
     <div className="App">
+      <div className='fileInput'>
+        <input onChange={onChangeFileHandler} type='file' name='file' />
+      </div>
+      {
+        error ?
+          <div className='error'>
+            {error}
+          </div>
+          : data
+      }
     </div>
   );
 }
