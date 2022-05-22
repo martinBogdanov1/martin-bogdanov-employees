f0baf57ac6fe83f54b884f920dc75d424f07aa12
martinBogdanov1 <martin.petrov1994@yahoo.com>
2022-05-22 21:46:26 +0300

    Implement core logic
    Includes major refactor - extract logic into functions. Adds end date checker.

--- src/App.js
+++ src/App.js
@@ -20,15 +20,96 @@ function App() {
     }
 
     parseData(inputFile);
+  }
 
-    function parseData(data) {
-      readString(data, {
-        worker: true,
-        complete: (results) => {
-          setData(results.data); 
-        }
-      });
+  function parseData(data) {
+    readString(data, {
+      worker: true,
+      complete: (results) => {
+        let employees = [];
+
+        results.data.forEach((x, i) => {
+          if (i > 0 && x[0] !== '') {
+
+            if (x[3] === 'NULL') {
+              x[3] = Date();
+            }
+
+            const employeeDetails = {
+              id: x[0],
+              projectId: x[1],
+              dateFrom: x[2],
+              dateTo: x[3]
+            };
+            employees.push(employeeDetails);
+          }
+        });
+
+        const employeesByProjectId = groupByProjectId(employees);
+        setPairDetails(getPairDetails(employeesByProjectId));
+      }
+    });
+  }
+
+  function getPairDetails(employees) {
+    let pairDetails = [];
+
+    for (let [projectId, employeesByProject] of Object.entries(employees)) {
+      if (employeesByProject.length < 2) {
+        continue;
+      }
+
+      if (employeesByProject.length === 2) {
+        setDaysWorkedPerEmployee(employeesByProject);
+        const daysWorkedByPair = employeesByProject[0].workDays + employeesByProject[1].workDays;
+
+        pairDetails.push(buildPairDetails(employeesByProject, projectId, daysWorkedByPair));
+      }
+
+      if (employeesByProject.length > 2) {
+        setDaysWorkedPerEmployee(employeesByProject);
+
+        employeesByProject.sort((a, b) => b.workDays - a.workDays);
+
+        const longestPair = employeesByProject.slice(0, 2);
+        const daysWorkedByPair = longestPair[0].workDays + longestPair[1].workDays;
+
+        pairDetails.push(buildPairDetails(longestPair, projectId, daysWorkedByPair));
+      }
     }
+
+    return pairDetails.sort((a, b) => b.days - a.days);
+  }
+
+  function setDaysWorkedPerEmployee(employees) {
+    employees.map(x => {
+      const daysWorked = dateRangeToDaysConverter(x.dateTo, x.dateFrom);
+      x.workDays = daysWorked
+    });
+  }
+
+  function buildPairDetails(groupedEmployees, projectId, daysWorked) {
+    return {
+      firstEmployeeId: groupedEmployees[0].id,
+      secondEmployeeId: groupedEmployees[1].id,
+      projectId: projectId,
+      daysWorked: daysWorked
+    }
+  }
+
+  function groupByProjectId(employees) {
+    return employees.reduce((group, employee) => {
+      if (!group[employee.projectId]) {
+        group[employee.projectId] = [];
+      }
+      group[employee.projectId].push(employee);
+
+      return group;
+    }, {});
+  }
+
+  function dateRangeToDaysConverter(dateTo, dateFrom) {
+    return Math.ceil((Date.parse(dateTo) - Date.parse(dateFrom)) / (1000 * 60 * 60 * 24));
   }
 
 
